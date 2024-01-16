ca2c87ee1a3b883fa4aef803b0a9589dfc5403ea
Joseph Villarreal Lopez <joseph.villarreal@66degrees.com>
2024-01-16 14:24:17 -0600

    chore: Update .gitignore and tf-variables.tf

--- infrastructure/terraform/tf-variables.tf
+++ infrastructure/terraform/tf-variables.tf
@@ -1,53 +1,53 @@
 variable "folder_display_name" {
   description = "Display name for the new folder."
   type        = string
-  default     = "genai-issue-actor"
+  default     = ""
 }
 
 variable "parent" {
   description = "google_folder_parent"
   type        = string
-  default     = "folders/247598731200"
+  default     = ""
 }
 
 variable "project_name" {
   description = "Name of the new project."
   type        = string
-  default     = "genai-issue-actor"
+  default     = ""
 }
 
 variable "project_id" {
   description = "The ID of the project in which resources will be created."
-  default     = "genai-issue-actor"
+  default     = ""
   type        = string
 }
 
 variable "billing_account" {
   description = "The ID of the organization."
   type        = string
-  default     = "012F00-93BFC2-44CBB0"
+  default     = ""
 }
 
 variable "region" {
   description = "The region to use for the created resources."
   type        = string
-  default     = "us-central1"
+  default     = ""
 }
 
 variable "github_owner" {
   description = "The region to use for the created resources."
   type        = string
-  default     = "Lapeyus"
+  default     = ""
 }
 
 variable "github_repo" {
   description = "The region to use for the created resources."
   type        = string
-  default     = "genai-issue-actor"
+  default     = ""
 }
 
 variable "llm" {
   description = "llm"
   type        = string
-  default     = "gemini-pro"
+  default     = ""
 }
