c89702f3c1cec05dcae0e5dc928f8562267f1074
lapeyus <lapeyus@gmail.com>
2024-01-24 01:20:34 +0000

    feat: Change site icon to a whale.
    Signed-off-by: lapeyus <lapeyus@gmail.com>

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ load_dotenv()
 REGION = os.getenv('REGION')
 PROJECT_ID = os.getenv('PROJECT_ID')
 
-st.set_page_config(page_title="Wynnsights", page_icon="👊", layout="wide", initial_sidebar_state="auto")
+st.set_page_config(page_title="Wynnsights", page_icon="🐋", layout="wide", initial_sidebar_state="auto")
 
 if 'dataframe' not in st.session_state:
     st.session_state['dataframe'] = {}
