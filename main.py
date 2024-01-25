da95bcfc9d24bd4ca51513e7b90c7184b5b54a22
lapeyus <lapeyus@gmail.com>
2024-01-25 22:58:24 +0000

    feat: Add retry mechanism for BigQuery query execution
    Signed-off-by: lapeyus <lapeyus@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import os
 import re
 
@@ -10,38 +11,46 @@ from langchain.document_loaders import BigQueryLoader
 from langchain.llms import VertexAI
 from langchain.prompts import PromptTemplate
 
+# Load environment variables
 load_dotenv()
 REGION = os.getenv("REGION")
 PROJECT_ID = os.getenv("PROJECT_ID")
 
+# Configure Streamlit page
 st.set_page_config(
     page_title="Wynnsights", page_icon="👊", layout="wide", initial_sidebar_state="auto"
 )
 
+# Initialize session state
 if "dataframe" not in st.session_state:
     st.session_state["dataframe"] = {}
 
+
+# Initialize BigQuery client
 client = bigquery.Client()
 
+
+# Sidebar for user input and configuration
 st.title("Wynnsights BQ")
 st.sidebar.title("Configuration")
-datset = st.sidebar.selectbox(
+
+# Dataset selection
+dataset = st.sidebar.selectbox(
     "Dataset", (f"{PROJECT_ID}.thelook", "bigquery-public-data.github_repos")
 )
+
+# Schema query
 schema_query = f"""
 SELECT table_name, ddl
-FROM `{datset}.INFORMATION_SCHEMA.TABLES`
+FROM `{dataset}.INFORMATION_SCHEMA.TABLES`
 WHERE table_type = 'BASE TABLE'
 ORDER BY table_name;
 """
-template = """SYSTEM: You are a bigquery specialist helping users by suggesting a GoogleSQL query that will help them answer their question againsts the provided context. Do not add backticks to your answer. make sure you reply with valid GoogleSQL syntax.\n
-=============
-Question: \n\n{question}
-=============
-context: \n\n{schema}
-"""
 
+# LLM model selection
 model_name = st.sidebar.selectbox("LLM Model Name", ("code-bison", "text-bison@002"))
+
+# LLM parameters
 max_output_tokens = st.sidebar.number_input(
     "Max Output Tokens", min_value=1, value=2048
 )
@@ -51,6 +60,8 @@ top_k = st.sidebar.number_input(
     "Top K (vocabulary probability)", min_value=0, max_value=1, value=1
 )
 verbose = st.sidebar.checkbox("Verbose", value=True)
+
+# Initialize LLM model
 llm = VertexAI(
     model_name=model_name,
     max_output_tokens=max_output_tokens,
@@ -59,14 +70,28 @@ llm = VertexAI(
     top_k=top_k,
     verbose=verbose,
 )
-prompt = PromptTemplate.from_template(template)
+
+# Initialize prompt template
+prompt = PromptTemplate.from_template(
+    """
+SYSTEM: You are a bigquery specialist helping users by suggesting a GoogleSQL query that will help them answer their question againsts the provided context. Do not add backticks to your answer. make sure you reply with valid GoogleSQL syntax.
+=============
+Question:
+{question}
+=============
+context:
+{schema}
+"""
+)
+
+# Chain the prompt and LLM model
 chain = prompt | llm
 
-
+# Initialize BigQuery loader
 loader = BigQueryLoader(query=schema_query, page_content_columns="ddl")
-data = loader.load()
 
 
+# Parse BigQuery schema from documents
 def parse_bigquery_schema(documents):
     result = []
 
@@ -92,6 +117,7 @@ def parse_bigquery_schema(documents):
     return "\n\n".join(result)
 
 
+# Format BigQuery dataframe for display
 def format_table(df):
     formatted_strings = []
     for row_name, row in df.iterrows():
@@ -100,55 +126,66 @@ def format_table(df):
     return ", ".join(formatted_strings)
 
 
+# Initialize Vertex AI client
 vertexai.init(project=PROJECT_ID, location=REGION)
 
+# Main app layout
 with st.container():
+    # Get user query
     user_query = st.text_input("Enter your query:")
+
     col1, col2 = st.columns(2)
 
+    # Data and schema expanders
     with col2:
         data_placeholder = col2.expander("Data", expanded=True).empty()
         schema_placeholder = col2.expander("BQ Schema", expanded=False).empty()
 
+        # Load data and schema
         data = loader.load()
         parsed_schema = parse_bigquery_schema(data)
         st.session_state["dataframe"] = parsed_schema
         schema_placeholder.write(parsed_schema)
 
-if col1.button("Generate Query"):
-    bqschema = parse_bigquery_schema(data)
-    googlesql = (
-        (chain.invoke({"question": user_query, "schema": bqschema}))
-        .strip("```")
-        .strip("googlesql")
-    )
-    col1.code(googlesql, language="sql", line_numbers=True)
-    custom_retry = Retry(
-        initial=1.0,
-        maximum=10.0,
-        multiplier=2,
-        deadline=300.0,
-    )
-    try:
-        bqdata = (
-            client.query(googlesql)
-            .result(timeout=300, retry=custom_retry)
-            .to_dataframe()
+    # Generate query button
+    if col1.button("Generate Query"):
+        bqschema = parse_bigquery_schema(data)
+        googlesql = (
+            (chain.invoke({"question": user_query, "schema": bqschema}))
+            .strip("```")
+            .strip("googlesql")
         )
-        data_placeholder.write(bqdata)
-        describe = "you are a data scientist, describe this and interpret the query intent in relation to the bigquery schema answering the question: what is this information good for?, very briefly evaluate the IA generated sql in terms of optimization, stricly limit your response to information in this context:\n"
-        col1.write(
-            llm(
-                describe
-                + "bigquery schema: "
-                + bqschema
-                + " user query: "
-                + user_query
-                + "generated sql:"
-                + googlesql
-                + "data response: "
-                + format_table(bqdata)
+        col1.code(googlesql, language="sql", line_numbers=True)
+
+        # Execute query with retry
+        custom_retry = Retry(
+            initial=1.0,
+            maximum=10.0,
+            multiplier=2,
+            deadline=300.0,
+        )
+        try:
+            bqdata = (
+                client.query(googlesql)
+                .result(timeout=300, retry=custom_retry)
+                .to_dataframe()
             )
-        )
-    except Exception as e:
-        st.error(f"Error executing query: {e}")
+            data_placeholder.write(bqdata)
+
+            # Generate description and interpretation of the query
+            describe = "you are a data scientist, describe this and interpret the query intent in relation to the bigquery schema answering the question: what is this information good for?, very briefly evaluate the IA generated sql in terms of optimization, stricly limit your response to information in this context:\n"
+            col1.write(
+                llm(
+                    describe
+                    + "bigquery schema: "
+                    + bqschema
+                    + " user query: "
+                    + user_query
+                    + "generated sql:"
+                    + googlesql
+                    + "data response: "
+                    + format_table(bqdata)
+                )
+            )
+        except Exception as e:
+            st.error(f"Error executing query: {e}")
