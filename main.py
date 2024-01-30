4c55699bb0ba019f4e82fadfadea765bb124f56f
lapeyus <lapeyus@gmail.com>
2024-01-30 14:40:35 +0000

    feat: update UI of the tool to include a funky chair icon in the page title
    Signed-off-by: lapeyus <lapeyus@gmail.com>

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ REGION = os.getenv("REGION")
 PROJECT_ID = os.getenv("PROJECT_ID")
 
 st.set_page_config(
-    page_title="Wynnsights", page_icon="👊", layout="wide", initial_sidebar_state="auto"
+    page_title="Wynnsights", page_icon="🪑", layout="wide", initial_sidebar_state="auto"
 )
 
 if "dataframe" not in st.session_state:
