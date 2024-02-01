5221e3e722a04f496d180f18976aaf4dabdc1885
Joseph Villarreal Lopez <joseph.villarreal@66degrees.com>
2024-01-31 18:03:03 -0600

    feat: Add initial version of test.py

---
+++ test.py
@@ -0,0 +1,39 @@
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+
+from langchain.output_parsers import PydanticOutputParser
+from langchain_core.pydantic_v1 import BaseModel, Field
+import os
+# from streamlit_feedback import streamlit_feedback
+from langchain_google_genai import ChatGoogleGenerativeAI
+import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+
+ 
+
+model = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key="AIzaSyCKfZBlOZs0FHcnX3TtUlHzr4ai6mhuAjQ")
+
+
+class GitBranch(BaseModel):
+    name: str = Field(description="valid name for a git branch")
+
+class GitFile(BaseModel):
+    file: str = Field(description="file contents")
+
+create_branch_prompt_template = PromptTemplate.from_template(
+                "Create a branch name based on it's purpose:\n{desired_change}\nand this guidelines:\n{contributing}. only reply with the branch name, don't add any explanation or aditional text, your response will be inserted in a git create branch command"
+            )
+parser = PydanticOutputParser(pydantic_object=GitBranch)
+template = "Create a branch name based on it's purpose:\n{desired_change}\nand this guidelines:\n{contributing}\nformat:{format_instructions}"
+prompt = PromptTemplate(
+    template=template,
+    input_variables=["desired_change","contributing"],
+    partial_variables={
+        "format_instructions": parser.get_format_instructions()
+    }
+)            
+chain = prompt | model| parser
+branch_name = chain.invoke({"desired_change": "add units tests", "contributing": "git best practices"}).name
+print(branch_name)
