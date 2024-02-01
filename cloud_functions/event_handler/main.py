6c1c084e6ffbb4d2bbd8e0e72aa37f05c92ef84a
lapeyus <lapeyus@gmail.com>
2024-02-01 18:23:41 +0000

    Update commit message to use conventional commits format
    Signed-off-by: lapeyus <lapeyus@gmail.com>

--- cloud_functions/event_handler/main.py
+++ cloud_functions/event_handler/main.py
@@ -16,7 +16,6 @@ pubsub_topic = get_env_variable("PUBSUB_TOPIC")
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(project_id, pubsub_topic)
 
-
 @functions_framework.http
 def handle_issue(request):
     request_json = request.get_json(silent=True)
@@ -24,29 +23,44 @@ def handle_issue(request):
 
     if "action" not in request_json:
         logger.info("Request body does not include an action")
-        return (
-            json.dumps({"err": 'Request body does not include an "action"'}),
-            400,
-            return_headers,
-        )
+        return (json.dumps({
+            "err": 'Request body does not include an "action"'
+        }),
+                400,
+                return_headers,
+            )
 
     if request_json["issue"]["assignee"] is not None:
-        logger.info(f"Issue already assigned to: {request_json['issue']['assignee']}")
-        return (json.dumps({"err": "Issue already assigned"}), 200, return_headers)
+        logger.info(
+f"Issue already assigned to: {request_json['issue']['assignee']}"
+        )
+        return (json.dumps({
+            "err": "Issue already assigned"
+        }),
+                200,
+                return_headers,
+            )
 
     if request_json["action"] != "opened":
         logger.info(
-            f"ignoring non-new issue notification, action: {request_json['action']}"
+f"ignoring non-new issue notification, action: {request_json['action']}"
         )
-        return (
-            json.dumps({"msg": "Ignoring non-new issue notification"}),
-            200,
-            return_headers,
-        )
+        return (json.dumps({
+            "msg": "Ignoring non-new issue notification"
+        }),
+                200,
+                return_headers,
+            )
     try:
         request_json["issue"]["body"] = parse_issue_body(request_json["issue"]["body"])
         publisher.publish(topic_path, json.dumps(request_json).encode("utf-8"))
         logger.info("Sent to processor queue")
-        return (json.dumps({"msg": "ok"}), 200, return_headers)
+        return (json.dumps({
+            "msg": "ok"
+        }),
+                200,
+                return_headers,
+            )
     except BaseException as e:
-        return (json.dumps({"err": str(e)}), 500, return_headers)
+        return (json.dumps({
+            "err": str(e)}), 500, return_headers)
